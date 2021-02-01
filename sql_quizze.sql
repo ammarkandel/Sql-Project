256a65ea84901bcc695cbb4cde3b7cdc7da09c22
ammarkandel <merom8703@gmail.com>
2021-02-01 17:04:12 +0200

    add using null quizze

--- sql_quizze.sql
+++ sql_quizze.sql
@@ -1029,3 +1029,89 @@ FROM
 WHERE
     a.company = 'LRT'
     AND stopa.name = 'Craiglockhart';
+
+/* Using Null */
+/* 1 */
+SELECT
+    name
+FROM
+    teacher
+WHERE
+    dept IS NULL;
+
+/* 2 */
+SELECT
+    teacher.name,
+    dept.name
+FROM
+    teacher
+    INNER JOIN dept ON (teacher.dept = dept.id);
+
+/* 3 */
+SELECT
+    teacher.name,
+    dept.name
+FROM
+    teacher
+    LEFT JOIN dept ON (teacher.dept = dept.id);
+
+/* 4 */
+SELECT
+    teacher.name,
+    dept.name
+FROM
+    teacher
+    RIGHT JOIN dept ON (teacher.dept = dept.id);
+
+/* 5 */
+SELECT
+    name,
+    COALESCE(mobile, '07986 444 2266')
+FROM
+    teacher;
+
+/* 6 */
+SELECT
+    teacher.name,
+    COALESCE(dept.name, 'None')
+FROM
+    teacher
+    LEFT JOIN dept ON teacher.dept = dept.id;
+
+/* 7 */
+SELECT
+    COUNT(teacher.id),
+    COUNT(mobile)
+FROM
+    teacher;
+
+/* 8 */
+SELECT
+    dept.name,
+    COUNT(teacher.id)
+FROM
+    teacher
+    RIGHT JOIN dept ON dept.id = teacher.dept
+GROUP BY
+    dept.name;
+
+/* 9 */
+SELECT
+    name,
+    CASE
+        WHEN dept IN(1, 2) THEN 'Sci'
+        ELSE 'Art'
+    END
+FROM
+    teacher;
+
+/* 10 */
+SELECT
+    name,
+    CASE
+        WHEN dept IN(1, 2) THEN 'Sci'
+        WHEN dept = 3 THEN 'Art'
+        ELSE 'None'
+    END
+FROM
+    teacher;
